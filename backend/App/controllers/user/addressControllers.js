ee0e83b7d01f9e9e446fe4eebb9138161d5f0547
Chandra6388 <cppatel6388@gmail.com>
2025-06-12 00:36:54 +0530

    done

--- backend/App/controllers/user/addressControllers.js
+++ backend/App/controllers/user/addressControllers.js
@@ -4,7 +4,31 @@ const db = require('../../models');
 const address = db.address;
 
 class Address {
-    async
+    async addAddress(req, res) {
+        const { userId, fullname, phone, pinCode, address, city, state } = req.body
+        if (!userId) {
+            return res.send({ status: false, message: "User id is require" })
+        }
+        if (!fullname) {
+            return res.send({ status: false, message: "Full name is require" })
+        }
+        if (!phone) {
+            return res.send({ status: false, message: "Phone number is require" })
+        }
+        if (!pinCode) {
+            return res.send({ status: false, message: "Pin code is require" })
+        }
+        if (!address) {
+            return res.send({ status: false, message: "address is require" })
+        }
+        if (!city) {
+            return res.send({ status: false, message: "city is require" })
+        }
+        if (!state) {
+            return res.send({ status: false, message: "state is require" })
+        }
+
+    }
 }
 
 
